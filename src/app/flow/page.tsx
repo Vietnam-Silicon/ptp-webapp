c43f2bcb61b4734d2cb3943771dc98532591393c
DESKTOP-J3A7I2P\Dang <dang.nguyen@vnsilicon.net>
2025-02-05 17:32:15 +0700

    Implement flow chart

--- src/app/flow/page.tsx
+++ src/app/flow/page.tsx
@@ -1,11 +1,11 @@
-import Chart from 'body/flow/chart';
-import Flow from 'body/flow/map';
+import Flow from 'body/flow/chart';
+import Map from 'body/flow/map';
 
 const Index = () => {
   return (
     <>
-      <Chart />
       <Flow />
+      <Map />
     </>
   );
 };
