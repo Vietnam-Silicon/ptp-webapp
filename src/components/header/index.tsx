994535b02dbe3799dcf3c2123ada76ff7dc920a6
Vuong Ng <vuong.nguyen@vnsilicon.net>
2025-02-10 11:57:02 +0700

    update css, convetion

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,19 +2,17 @@ import Drawer from './drawer';
 import Logo from './logo';
 import "./styles.css";
 
-const Index = () => {
-  return (
-    <div className="container">
-      <div className="left">
-        <Drawer />
-        <Logo />
-      </div>
-      <div className="right">
-        <img className="avatar" src="./avatar.png" alt="Avatar" />
-        <div className="name">Stebin Ben</div>
-      </div>
+const Index = () => (
+  <div className="container">
+    <div className="left">
+      <Drawer />
+      <Logo />
     </div>
-  );
-};
+    <div className="right">
+      <img className="avatar" src="./avatar.png" alt="Avatar" />
+      <div className="name">Stebin Ben</div>
+    </div>
+  </div>
+);
 
 export default Index;
