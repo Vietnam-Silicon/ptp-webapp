728228259efb86fd71cb0f9038d5bb28a70d9e32
Vuong Ng <vuong.nguyen@vnsilicon.net>
2025-02-10 11:47:50 +0700

    update css

--- src/components/header/logo/index.tsx
+++ src/components/header/logo/index.tsx
@@ -1,14 +1,11 @@
-import Link from 'next/link';
-
 import './styles.css';
 
 const Index = () => (
   <div className="logo-container">
     <img className="logo" src="./logo.png" alt="BKS" />
-    <Link className="brand-name" href="/">BKS</Link>
-    {/* <span className="brand-name">
+    <span className="brand-name">
       BKS
-    </span> */}
+    </span>
   </div>
 );
 
