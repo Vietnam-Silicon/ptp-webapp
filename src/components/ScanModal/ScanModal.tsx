0635d196e483c1c8f3998226d735facf2fd912cc
DESKTOP-J3A7I2P\Dang <dang.nguyen@vnsilicon.net>
2025-02-10 10:19:00 +0700

    Implement client page

--- src/components/ScanModal/ScanModal.tsx
+++ src/components/ScanModal/ScanModal.tsx
@@ -1,9 +1,9 @@
 'use-client';
 
 import * as React from 'react';
-import { Box, Button, Drawer } from '@mui/material';
+import { Box, Modal, Typography, IconButton } from '@mui/material';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import { Result } from '@zxing/library';
-
 import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 
 type ScannerProps = {
@@ -28,39 +28,58 @@ interface ScanModalProps {
 
 export const ScanModal = (props: ScanModalProps) => {
   return (
-    <Drawer
-      sx={{
-        width: '100%',
-        maxWidth: '500px',
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-          width: '100%  ',
-          boxSizing: 'border-box',
-          height: '100%',
-          padding: '20px',
+    <Modal open={props.open} onClose={props.onClose} disableScrollLock>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '100%',
           maxWidth: '500px',
+          height: '100%',
+          boxSizing: 'border-box',
           backgroundColor: '#555555',
-        },
-      }}
-      disablePortal
-      PaperProps={{ height: '100%' }}
-      anchor={'bottom'}
-      open={props.open}
-      onClose={props.onClose}
-    >
-      <Scanner
-        delay={200}
-        onUpdate={(err, result) => {
-          if (result) {
-            props.onScan(result.getText());
-            props.onClose();
-          }
+          margin: 0,
+          padding: '0 20px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
-      />
+      >
+        <Box>
+          <Typography fontSize="20px" align="center" sx={{ color: '#fff' }} mb="8px">
+            Scan the QR/barcode
+          </Typography>
+          <Box
+            sx={{
+              borderRadius: '12px',
+              overflow: 'hidden',
+              border: '2px solid #fff',
+              mb: '32px',
+              '& video': {
+                display: 'block',
+              },
+            }}
+          >
+            <Scanner
+              onUpdate={(err, result) => {
+                if (result) {
+                  props.onScan(result.getText());
+                  props.onClose();
+                }
+              }}
+            />
+          </Box>
+        </Box>
 
-      <Button variant="contained" color="primary" fullWidth onClick={props.onClose}>
-        Close
-      </Button>
-    </Drawer>
+        <IconButton
+          sx={{ color: '#fff', position: 'absolute', right: '10px', top: '10px', fontSize: '36px' }}
+          onClick={props.onClose}
+        >
+          <CancelOutlinedIcon fontSize="inherit" />
+        </IconButton>
+      </Box>
+    </Modal>
   );
 };
