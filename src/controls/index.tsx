7e040a9a05785d0aa86494387ad26377c68d5ed2
minhnhutVNSilicon <nhut.ngo@vnsilicon.net>
2025-02-12 16:46:26 +0700

    fix conflict

--- src/controls/index.tsx
+++ src/controls/index.tsx
@@ -5,8 +5,4 @@ import { TimePicker } from './TimePicker';
 import { Dropdown } from './Dropdown';
 import { Button } from './Button';
 
-<<<<<<< HEAD
-export { Input, DatePicker, TimePicker, Dropdown, Button };
-=======
-export { Input, DatePicker, TimePicker, DebounceInput, Select, MenuItem };
->>>>>>> 7c37c3c (update filter)
+export { Input, DatePicker, TimePicker, Dropdown, Button, DebounceInput, Select, MenuItem };
